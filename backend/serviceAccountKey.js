6ee15473c957403604bacc91a57925c1ec0da4ae
Hani☠️ <hanija.lc240073@gmail.com>
2026-09-18 19:00:28 +0530

    Add service account key for Firebase

---
+++ backend/serviceAccountKey.js
@@ -0,0 +1,13 @@
+{
+  "type": "service_account",
+  "project_id": "juicy1-96e7b",
+  "private_key_id": "ab4d3efc1403da6daebd08a12555b54405a6e7eb",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCs8YHn92g23WHm\nnMxawCqwY+OYoSRSPGxdTaVXR8M24G2vSjMJqWZe20nKqT8XLQUoAbQw+RvQpCEf\nwqjDvIqPdLUPnXZHQLKBvKnEVRIgi6v7+WHerk6EIfkmjcyTxvNhYXKVnbUgnE9+\nss/0WMleuKtWomz+L6HbCzEPx5BjmqMPFTMnjGVUfZYH/NFpt+Nttp2Bm9mL5JkT\ngVZbVecZ1cN3+gVsif8dH2gAmRTpmFlmJ8bdGycgmaIxcmyNXl+GGYUN/wCpVGp9\nLgIF2ZphA5wMBNLDowXZaJm1gPeApO5kbl01s9wTVWdR4cvDSxM+3Mgo53CyNtGX\nwYzkophnAgMBAAECggEAUGRJd0t+IbbiOgGcSvk2Pt2IIFyT1jhpRSwBVmOw/rLv\nucKbm/unbyOvlyn6fmr6kSDNotBBmkiZbzqdGbPskwf03KfdIf0UnZNWNUI4VXGn\nxk8kLA/JxslGGKJkvTMH0rYqPhtb5adTmrlDSrn1DXd5iiGh2bzJl9mJemTRnUKy\ntkaLm3oM9tQjEile7ZBAYgEIKh1Syrn1h2ztnPrFjn68QGvY8NZcqDs39/il8yRt\ndWHToRFdBtQzdcTxBUT6CB5Ac4vWMda0VGVSTkaKAHcHzCIT5GP+o+TYzXoHlsMB\nUkuu/NvT4G1LUzASq7gWg7Y4O1Kh6UlhePv9ar8UbQKBgQDT8KErKMOnNcaquTmS\nrKJ2wlLiqpxB+q7xz/bB1GTawSPv1bRekGLTqIFiN0r0txJywAlTNOY2kqVyjaLY\nmbC+B0mOg25VnRGpATcuJOXLuc5c18RWKI5W+cfYqM2rYJmF2DjqgynEnc01wIW6\nxahbEl9v/mA/EZygU7zR4dtOLQKBgQDQ5X55JPvfYHKpHxIXfe2BY8LepiEq/R8l\nu7qSF0jkGec+cRU98sjX9uswDqKOzWZSILLV954Ipi2ts3q/yRc6OAz2IqpM15LK\nd2iJ0xJRJ7oD/3FhdIpVCR50zfAoO4jwckBNeIVavMTWaw8AaKPdUEKbOTbAhUkB\nuK0x6QfxYwKBgBrRl0Nj3CY7itEtsRhbWg+XFW/zUOSUeTDGNR5f5wc7a3W84B8b\nPDY2W3D3ODfxNpdor4Vk60C6m878F0A7an8RYwBypzKUNpjhO39OUfnOoEtlTkXs\nyd49q0lCSwxFpAeozcL3KADmSwJjQ0jYHC6yC3ubf8hbTSuIjRst7lddAoGAA/Sk\njXS21EcGRM+SzyESU0XpVb/9lsvOQC+q28NXHQKkj1ki1tZ3GSfS0kkkqOGY02tN\nF6IluvIyoTxp0aHkL7UKAcssQGJApFbYguudSC4hZpv6sKa9S9jtW7bemlRuRmdT\ncmQa2loCvDCwywgwOiuf50KRF9ouRLsThINuJBMCgYEAq/WHkYjpyL2cyi3BCjfD\ng7bhgeej3cw4Igx2jqdnogCVclj0INBXqPcWfZ3AYe43CzTK0v2ms/JWgMkHR74h\nq6LIzsNiialU4YjQe1s64fCj98KndOOoQ8ZcvMqkGIE/JeTHJMESHI9tfHZXnQop\nmiFfsUyGfRMfKAy5DtHJwks=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@juicy1-96e7b.iam.gserviceaccount.com",
+  "client_id": "111876729169551705044",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40juicy1-96e7b.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
